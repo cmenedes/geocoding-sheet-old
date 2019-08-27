fb06966e012e04bbc38918a6e2c9a832bc39c983
timkeane <tkeane@doitt.nyc.gov>
2019-08-27 19:46:18 -0400

    tests

--- __tests__/Code.test.js
+++ __tests__/Code.test.js
@@ -4,11 +4,18 @@ import path from 'path'
 import google from './goog.mock'
 import HtmlService from './HtmlService.mock'
 import SpreadsheetApp from './SpreadsheetApp.mock'
+import goog from './goog.mock';
 
-const codeJs = fs.readFileSync(path.resolve(__dirname, '../src/js/Code.gs'), {encoding: 'utf-8'})
+const gs = fs.readFileSync(path.resolve(__dirname, '../src/js/Code.gs'), {encoding: 'utf-8'})
 
 beforeAll(() => {
-  $('body').append(`<script type="text/javascript">${codeJs}</script>`)
+  $('body').append(`<script type="text/javascript">${gs}</script>`)
+})
+
+beforeEach(() => {
+  HtmlService.resetMocks()
+  SpreadsheetApp.resetMocks()
+  google.resetMocks()
 })
 
 test('functions loaded', () => {
@@ -22,4 +29,43 @@ test('functions loaded', () => {
   expect(typeof geoCols).toBe('function')
   expect(typeof setFields).toBe('function')
   expect(typeof geocoded).toBe('function')
+})
+
+describe('onInstall', () => {
+  let onOpen_
+  beforeEach(() => {
+    onOpen_ = onOpen
+    onOpen = jest.fn()
+  })
+  afterEach(() => {
+    onOpen = onOpen_
+  })
+
+  test('onInstall', () => {
+    expect.assertions(1)
+
+    onInstall()
+    
+    expect(onOpen).toHaveBeenCalledTimes(1)
+  })
+})
+
+test('onOpen', () => {
+  expect.assertions(6)
+
+  onOpen()
+
+  expect(SpreadsheetApp.getUi).toHaveBeenCalledTimes(1)
+  
+  const ui = SpreadsheetApp.getUi.mock.results[0].value
+  expect(ui.createAddonMenu).toHaveBeenCalledTimes(1)
+
+  const menu = ui.createAddonMenu.mock.results[0].value
+  expect(menu.addItem).toHaveBeenCalledTimes(1)
+  expect(menu.addItem.mock.calls[0][0]).toBe('Geocoder')
+  expect(menu.addItem.mock.calls[0][1]).toBe('show')
+
+  const item = menu.addItem.mock.results[0].value
+  expect(item.addToUi).toHaveBeenCalledTimes(1)
+
 })
